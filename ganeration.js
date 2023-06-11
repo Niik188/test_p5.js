4956d9d26bba03a435c07385b1a1bc2027062810
Niik18 <ermolin.nik12@mail.com>
2023-06-11 07:27:20 +0300

    test 0.2

--- ganeration.js
+++ ganeration.js
@@ -3,12 +3,17 @@ class Map{
         this.position = position
         this.size = size
         this.background = background
+        this.ground = {x:0, y:0, w:0, h:0}
     }
     setup(){
         this.background = loadImage(this.background)
+        this.ground.y = 364
+        this.ground.w = this.size.w
+        this.ground.h = 20
     }
     draw(){
         image(this.background, this.position.x, this.position.y, this.size.w, this.size.h)
+        rect(this.ground.x,this.ground.y,this.ground.w,this.ground.h)
     }
 }
 
--- physics.js
+++ physics.js
@@ -1,9 +1,19 @@
 import { player } from "./Player.js";
+import { room } from "./ganeration.js";
 
 export function physics() {
     player.position.y += player.velocity.y
     player.velocity.y += 0.5
-    if (player.position.y + player.size.h > 550) {
-        player.velocity.y = 0
+    if (collision(player,room.ground)) {
+        if (player.velocity.y > 0) {
+            player.velocity.y = 0
+        }
     }
+}
+
+export function collision(player, object2) {
+    return (player.position.x+player.size.w >= object2.x &&
+            player.position.x <= object2.x+object2.w&&
+            player.position.y+player.size.h >= object2.y&&
+            player.position.y <= object2.y+object2.h)
 }
